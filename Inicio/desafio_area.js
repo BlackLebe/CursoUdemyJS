4d1762ee711666c827c13c83d9eddb24caa8f289
Calebe Bastos <calebehonorato@outlook.com>
2024-10-02 17:40:11 -0300

    Desafios que eu fiz, nada muito difícil, foi só pra acompanhar a aula.

---
+++ Inicio/desafio_area.js
@@ -0,0 +1,6 @@
+let raio = 8;
+const pi = 3.14;
+let operacao =  pi * raio * raio;
+
+console.log('A área dessa circunferência é: '+operacao+'m²');
+console.log(Math.PI);
---
+++ Inicio/desafio_sentenca_bloco.js
@@ -0,0 +1,18 @@
+/* o desafio consiste em somente separar os números pares e ímpares de 1 a 10 em blocos diferentes
+    Muito simples
+ */
+{
+    console.log(1);
+    console.log(3);
+    console.log(5);
+    console.log(7);
+    console.log(9);
+}
+
+{
+    console.log(2);
+    console.log(4);
+    console.log(6);
+    console.log(8);
+    console.log(10);
+}
